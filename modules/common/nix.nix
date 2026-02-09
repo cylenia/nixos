a0534d382b1b305135990e2d18db9342df39d004
cylenia <me@cylenia.dev>
2026-02-09 14:54:27 +1100

    lix

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,4 +1,5 @@
-{  ... }: {
+{ pkgs, ... }: {
+  nix.package = pkgs.lixPackageSets.stable.lix;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 }
