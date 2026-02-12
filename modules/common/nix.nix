3232264ff7f8e0dbc7334219ba170d7763c572c8
cylenia <me@cylenia.dev>
2026-02-12 19:20:47 +1100

    grub

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }: {
-  nix.package = pkgs.lixPackageSets.stable.lix;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
+  nix.package = pkgs.lixPackageSets.stable.lix;
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    warn-dirty = false;
+  };
 }
