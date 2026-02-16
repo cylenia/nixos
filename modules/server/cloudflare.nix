e9bf0f62326a3121bf418cbb89c6be0dd740526c
Cylenia <me@cylenia.dev>
2026-02-16 14:41:46 +1100

    cloudflare

--- modules/server/cloudflare.nix
+++ modules/server/cloudflare.nix
@@ -1,5 +1,12 @@
-{ ... }: {
+{ config, ... }: {
+  sops.secrets.cloudflared_creds = {};
   services.cloudflared = {
     enable = true;
+    tunnels = {
+      "d72e6609-f673-4b3c-ba51-b497906820bc" = {
+        credentialsFile = "${config.sops.secrets.cloudflared_creds.path}";
+        default = "http_status:404";
+      };
+    };
   };
 }
