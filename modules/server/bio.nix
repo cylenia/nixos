88ea6fb272d490e26fc0d0156ef6aae679bc4dd2
Cylenia <me@cylenia.dev>
2026-02-17 22:41:26 +1100

    elysia

--- modules/server/bio.nix
+++ modules/server/bio.nix
@@ -4,6 +4,7 @@ let
     owner = "cylenia-xd";
     repo = "bio";
     rev = "d1504ac88db272dc530c90c06a6acb60c7053ad6";
+    hash = "sha256-Mn34hPsc6hlY66hhkDnXDcyj+HFM5MFJkp0ETsusvBQ=";
   }) {};
 in {
   systemd.services.bio = {
