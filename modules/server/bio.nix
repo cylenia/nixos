7aefddac347044eee11ae2fbcd40942932eea8f8
Cylenia <me@cylenia.dev>
2026-02-21 15:47:01 +1100

    shitstir

--- modules/server/bio.nix
+++ modules/server/bio.nix
@@ -3,8 +3,7 @@ let
   bioSite = pkgs.callPackage (pkgs.fetchFromGitHub {
     owner = "cylenia-xd";
     repo = "bio";
-    rev = "6dc313edc2dcea3ac9c7a65c84c4dd1139ed083c";
-    hash = "sha256-T21GJDBZLZ5tlaOCM20aTc1R5RF2nVz+n/vfTtnOYZg=";
+    rev = "e6c392636ef43d824deb2a73d248cf75c385ae3f";
   }) {};
 in {
   systemd.services.bio = {
