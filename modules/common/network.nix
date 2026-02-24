b370da4f7710f63d7f7fc2c3ba75846f1605ec0f
Cylenia <me@cylenia.dev>
2026-02-24 23:09:03 +1100

    1.1.1.1

--- modules/common/network.nix
+++ modules/common/network.nix
@@ -2,7 +2,7 @@
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
   networking.nameservers = [
-    "168.138.12.137"
-    "112.213.35.91"
+    "1.1.1.1"
+    "1.0.0.1"
   ];
 }
