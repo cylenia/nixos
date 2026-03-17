9412a6cde0011474cd250360c5d9cc733d2f8027
cylenia <me@cylenia.dev>
2026-03-17 16:46:06 +1100

    homeassistant

--- modules/server/homeassistant.nix
+++ modules/server/homeassistant.nix
@@ -1,6 +1,7 @@
 { ... }: {
   services.home-assistant = {
     enable = true;
+    config.http.server_port = 8068;
     config.homeassistant = {
       name = "Home";
       time_zone = "Australia/Melbourne";
