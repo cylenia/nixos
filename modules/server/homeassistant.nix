ef20f64b92a8260841220b5e5f28a552b91129ff
cylenia <me@cylenia.dev>
2026-03-17 17:04:04 +1100

    remove config in that

--- modules/server/homeassistant.nix
+++ modules/server/homeassistant.nix
@@ -2,13 +2,5 @@
   services.home-assistant = {
     enable = true;
     config.http.server_port = 8068;
-    config.homeassistant = {
-      name = "Home";
-      time_zone = "Australia/Sydney";
-      latitude = -37.8;
-      longitude = 144.9;
-      unit_system = "metric";
-      temperature_unit = "C";
-    };
   };
 }
