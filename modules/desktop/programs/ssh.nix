cd28097f4ca3141917e31fe60f9c7dcea9ab1617
cylenia <me@cylenia.dev>
2026-04-13 19:13:03 +1000

    change ip for paul

--- modules/desktop/programs/ssh.nix
+++ modules/desktop/programs/ssh.nix
@@ -3,14 +3,8 @@
     enable = true;
     enableDefaultConfig = false;
     matchBlocks = {
-      greg = {
-        hostname = "192.227.173.19";
-        port = 22;
-        user = "root";
-        setEnv.TERM = "xterm-256color";
-      };
       paul = {
-        hostname = "192.168.86.50";
+        hostname = "192.168.86.36";
         port = 22;
         user = "cylenia";
         setEnv.TERM = "xterm-256color";
